314eb24e510c90d329fd00cf381ac084bf4d1ac5
Robinson Amianda <robinsonamianda5@gmail.com>
2023-10-08 13:15:54 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
+const array1 =  [3, 8, 12, 4, 11, 7,10]
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+ return true 
 }
-
+const array2 = [1, 2, 5,4] 
+function hasTargetSum(array, target) {
+ return false 
+}
+const array3 = [22, 19, 4, 6, 30,25] 
+function hasTargetSum(array, target) {
+ return true 
+}
+const array4 = [-7, 10, 4, 8,3] 
+function hasTargetSum(array, target) {
+ return true 
+}
+const array5 = [1, 2, 3, 4,5] 
+function hasTargetSum(array, target) {
+ return true
+} 
+const array6 =[2, 2, 3, 3,4] 
+function hasTargetSum(array, target) {
+ return true
+} 
 /* 
   Write the Big O time complexity of your function here
 */
